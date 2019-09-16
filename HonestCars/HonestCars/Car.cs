4150b8947a3f5e89e08d84b5bb14d0ec0adcaa7e
Dylan Beattie <dylan@dylanbeattie.net>
2019-09-16 15:10:49 +0100

    Working model with car manufacturers, models and variants

--- HonestCars/HonestCars/Car.cs
+++ HonestCars/HonestCars/Car.cs
@@ -1,28 +1,96 @@
 using System;
 
-namespace HonestCars {
-    class Car {
+namespace HonestCars.Model {
 
-      
-      // private decimal price;
-      //   public decimal Price { 
-      //     get { return price; } 
-      //     set { 
-      //       Console.WriteLine("CAR PRICE CHANGE ALERT!");
-      //       Console.WriteLine("Old price: " + this.price);
-      //       this.price = value;
-      //       Console.WriteLine("New price: " + this.price);
-      //     } 
-      //   }
-
-        // BLAH BLAH Java style. Don't like it.
-        // private decimal price; 
-
-        // public void SetPrice(decimal p) {
-        //     this.price = p;
-        // }
-        // public decimal GetPrice() {
-        //     return this.price;
-        // }
+  public class Manufacturer {
+    private string ChassisNumberPrefix {
+      get {
+        return this.Name.Substring(0,4).ToUpper();
+      }
     }
+
+    public string Name { get; set; }
+    public CarModel LaunchNewModel(string name) {
+      var model = new CarModel();
+      model.Make = this;
+      model.Name = name;
+      return model;
+    }
+
+    public Variant CreateVariant(CarModel model, int engineSize) {
+      return new Variant {
+        Model = model,
+        EngineSize = engineSize,
+      };      
+    }
+
+    public Car CreateCar(Variant variant, string partialChassisNumber) {
+      var fullChassisNumber = this.ChassisNumberPrefix + partialChassisNumber;
+      return new Car {
+        Variant = variant,
+        ChassisNumber = fullChassisNumber
+      };
+    }
+
+    public override string ToString() {
+      return Name;
+    } 
+  }
+
+  public class CarModel {
+    public Manufacturer Make { get; set;}
+    public string Name { get; set; }
+    
+    public override string ToString() {
+      return $"{Make} {Name}";
+    }
+  }
+
+  public class Variant {
+    public CarModel Model { get; set; }
+    public int EngineSize  { get; set; }
+
+    public override string ToString() {
+      return $"{Model}, {EngineSize} cc";
+    }
+  }
+
+  public class Car {
+    public Variant Variant { get; set; }
+    public string ChassisNumber { get; set; }
+
+    public override string ToString() {
+      return $"{ChassisNumber} ({Variant})";
+    }
+  }
+
+  // public class Car {
+  //     public string Make { get; }
+  //     public string Model { get; }      
+
+  //     public decimal Price { get; set; }
+  //     public int Mileage { get; set; }
+  //     public string Color { get; set; }
+  //     public int Year { get; set; }
+
+  //     public override string ToString() {
+  //       return $"{Make} {Model} ({Year}, {Mileage} miles) {Price:C}";
+  //     }
+
+  //     public Car(string make, string model) {
+  //       this.Make = make;
+  //       this.Model = model;
+  //     }
+
+  //     public Car(string make, string model, string color, int year, 
+  //       int mileage, decimal price) {
+  //       this.Make = make;
+  //       this.Model = model;
+  //       this.Color = color;
+  //       this.Year = year;
+  //       this.Mileage = mileage;
+  //       this.Price = price;
+  //     }
+  //   }
 }
+        
--- HonestCars/HonestCars/Program.cs
+++ HonestCars/HonestCars/Program.cs
@@ -1,14 +1,23 @@
 ﻿using System;
+using HonestCars.Model;
 
 namespace HonestCars {
     class Program
     {
         static void Main(string[] args)
         {
-            var c = new Car();
-            c.Price = 123.45M;
-            c.Price = 45678.9M;
-            Console.WriteLine(c.Price);
+            var vauxhall = new Manufacturer { Name = "Vauxhall" };
+            var astra = vauxhall.LaunchNewModel("Astra");
+            var astra1400 = vauxhall.CreateVariant(astra, 1400);
+            var astra1600 = vauxhall.CreateVariant(astra, 1600);
+
+            var car1 = vauxhall.CreateCar(astra1400, "123ABC");
+            var car2 = vauxhall.CreateCar(astra1600, "456DEF");
+
+            Console.WriteLine(car1);
+            Console.WriteLine(car2);
         }
     }
 }
+
+
